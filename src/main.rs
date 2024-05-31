9f2e4a9aae876b887e556f1a5d21821ca69434e3
Shivansh Bhatnagar <shivansh.bhatnagar.mat22@itbhu.ac.in>
2024-05-31 20:59:33 +0530

    Extract URL Path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
-use std::io::Write;
+use std::io::{BufReader, BufRead, Write};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,9 +12,8 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                println!("accepted new connection");
-                let _ = _stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
+            Ok(stream) => {
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -22,3 +21,16 @@ fn main() {
         }
     }
 }
+
+pub fn handle_connection (mut stream: std::net::TcpStream) {
+    let reader = BufReader::new(&mut stream);
+    let http_request = reader.lines().next().unwrap().unwrap();
+    let status_line = match &http_request[..] {
+        "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
+        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+    };
+    stream
+        .write_all(status_line.as_bytes())
+        .expect("Failed to write to stream");
+}
+
