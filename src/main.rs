bb1baa8e4b6ee599c81ba57694afb8b7d9c5deef
Shivansh Bhatnagar <shivansh.bhatnagar.mat22@itbhu.ac.in>
2024-06-01 15:16:13 +0530

    Respond with a body

--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,26 @@ fn main() {
         }
     }
 }
-
-pub fn handle_connection (mut stream: std::net::TcpStream) {
+pub fn handle_connection(mut stream: std::net::TcpStream) {
     let reader = BufReader::new(&mut stream);
     let http_request = reader.lines().next().unwrap().unwrap();
-    let status_line = match &http_request[..] {
-        "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
-        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+    let streq: Vec<&str> = http_request.split_whitespace().collect();
+    let mut command = "".to_string();
+    let mut path = "".to_string();
+    if streq[1][1..].contains('/') {
+        command = streq[1][1..streq[1][1..].find('/').unwrap()+1].to_string();
+        path = streq[1][(streq[1][1..].find("/").unwrap() + 2)..].to_string();
+    } else {
+        command = streq[1][1..].to_string();
     };
+    println!("{} {}", command, path);
+    let mut status_line = match &http_request[..] {
+        "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
+        _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
+    };
+    if command == "echo" {
+        status_line = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", path.len(), path);
+    }
     stream
         .write_all(status_line.as_bytes())
         .expect("Failed to write to stream");
